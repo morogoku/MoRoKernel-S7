ce2757654afbef17ddbad44131ebd5b0274ad5a9
Vinayak Menon <vinmenon@codeaurora.org>
2015-02-25 19:43:59 +0530

    mm: swap: don't delay swap free for fast swap devices
    There are couple of issues with swapcache usage when ZRAM is used
    as swap device.
    1) Kernel does a swap readahead which can be around 6 to 8 pages
    depending on total ram, which is not required for zram since
    accesses are fast.
    2) Kernel delays the freeing up of swapcache expecting a later hit,
    which again is useless in the case of zram.
    3) This is not related to swapcache, but zram usage itself.
    As mentioned in (2) kernel delays freeing of swapcache, but along with
    that it delays zram compressed page free also. i.e. there can be 2 copies,
    though one is compressed.
    
    This patch addresses these issues using two new flags
    QUEUE_FLAG_FAST and SWP_FAST, to indicate that accesses to the device
    will be fast and cheap, and instructs the swap layer to free up
    swap space agressively, and not to do read ahead.
    
    Change-Id: I5d2d5176a5f9420300bb2f843f6ecbdb25ea80e4
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -521,6 +521,7 @@ struct request_queue {
 #define QUEUE_FLAG_INIT_DONE   20	/* queue is initialized */
 #define QUEUE_FLAG_NO_SG_MERGE 21	/* don't attempt to merge SG segments*/
 #define QUEUE_FLAG_SG_GAPS     22	/* queue doesn't support SG gaps */
+#define QUEUE_FLAG_FAST        23	/* fast block device (e.g. ram based) */
 #ifdef CONFIG_JOURNAL_DATA_TAG
 #define QUEUE_FLAG_JOURNAL_TAG     31      /* supports JOURNAL_DATA_TAG */
 #endif
@@ -611,6 +612,7 @@ static inline void queue_flag_clear(unsigned int flag, struct request_queue *q)
 #define blk_queue_discard(q)	test_bit(QUEUE_FLAG_DISCARD, &(q)->queue_flags)
 #define blk_queue_secdiscard(q)	(blk_queue_discard(q) && \
 	test_bit(QUEUE_FLAG_SECDISCARD, &(q)->queue_flags))
+#define blk_queue_fast(q)	test_bit(QUEUE_FLAG_FAST, &(q)->queue_flags)
 
 #ifdef CONFIG_JOURNAL_DATA_TAG
 #define blk_queue_journal_tag(q)   test_bit(QUEUE_FLAG_JOURNAL_TAG, &(q)->queue_flags)

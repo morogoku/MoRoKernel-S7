b5fc97f12efc249ed0b54f63a5572b5e9194e236
morogoku <morogoku@hotmail.com>
2016-10-03 00:00:24 +0200

    fix clean.sh

--- clean.sh
+++ clean.sh
@@ -6,24 +6,24 @@ make clean
 make ARCH=arm64 distclean
 
 # Remove Build Log
-[ -f "$PWD/build.log" ] && rm -f $PWD/build.log
+rm -f $PWD/build.log
 
 # Remove Created Ramdisk Files
-#[ -f "$PWD/ramdisk/G930F/image-new.img" ] && rm -f $PWD/ramdisk/G930F/image-new.img
-#[ -f "$PWD/ramdisk/G930F/ramdisk-new.cpio.gz" ] && rm -f $PWD/ramdisk/G930F/ramdisk-new.cpio.gz
-#[ -f "$PWD/ramdisk/G930F/split_img/boot.img-dtb" ] && rm -f $PWD/ramdisk/G930F/split_img/boot.img-dtb
-#[ -f "$PWD/ramdisk/G930F/split_img/boot.img-zImage" ] && rm -f $PWD/ramdisk/G930F/split_img/boot.img-zImage
-[ -f "$PWD/ramdisk/G935F/image-new.img" ] && rm -f $PWD/ramdisk/G930F/image-new.img
-[ -f "$PWD/ramdisk/G935F/ramdisk-new.cpio.gz" ] && rm -f $PWD/ramdisk/G935F/ramdisk-new.cpio.gz
-[ -f "$PWD/ramdisk/G935F/split_img/boot.img-dtb" ] && rm -f $PWD/ramdisk/G935F/split_img/boot.img-dtb
-[ -f "$PWD/ramdisk/G935F/split_img/boot.img-zImage" ] && rm -f $PWD/ramdisk/G935F/split_img/boot.img-zImage
+#rm -f $PWD/ramdisk/G930F/image-new.img
+#rm -f $PWD/ramdisk/G930F/ramdisk-new.cpio.gz
+#rm -f $PWD/ramdisk/G930F/split_img/boot.img-dtb
+#rm -f $PWD/ramdisk/G930F/split_img/boot.img-zImage
+rm -f $PWD/ramdisk/G930F/image-new.img
+rm -f $PWD/ramdisk/G935F/ramdisk-new.cpio.gz
+rm -f $PWD/ramdisk/G935F/split_img/boot.img-dtb
+rm -f $PWD/ramdisk/G935F/split_img/boot.img-zImage
 
 # Remove Releasetool files
-[ -f "$PWD/releasetools/zip/*.zip" ] && rm -f $PWD/releasetools/zip/*.zip
-[ -f "$PWD/releasetools/tar/*.tar" ] && rm -f $PWD/releasetools/tar/*.tar
+rm -f $PWD/releasetools/zip/*.zip
+rm -f $PWD/releasetools/tar/*.tar
 
 # Removed Created dtb Folder
-[ -d "$PWD/arch/arm64/boot/dtb" ] && rm -rf $PWD/arch/arm64/boot/dtb
+rm -rf $PWD/arch/arm64/boot/dtb
 
 # Recreate Ramdisk Placeholders
 #echo "" > ramdisk/G930F/ramdisk/data/.placeholder

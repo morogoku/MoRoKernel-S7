be570d9e0cdd0eec619a0c3d411397e375803b34
morogoku <morogoku@hotmail.com>
2016-10-23 22:44:44 +0200

    ramdisk: update kernel-init to moro-init

--- ramdisk/G935F/ramdisk/sbin/moro-init.sh
+++ ramdisk/G935F/ramdisk/sbin/moro-init.sh
@@ -1,16 +1,14 @@
 #!/system/bin/sh
 # 
-# Credits & Thanks for the script to:
-# Jesse Chan (Jesec)
-# Luca Grifo (Tkkg1994)
+# Init MoRoKernel
 #
 
-# Initial
-mount -o remount,rw -t auto /
-mount -t rootfs -o remount,rw rootfs
+
+# Mount
 mount -o remount,rw -t auto /system
-mount -o remount,rw /data
-mount -o remount,rw /cache
+mount -o remount,rw -t auto /data
+mount -t rootfs -o remount,rw rootfs
+
 
 # Google play services wakelock fix
 sleep 3
@@ -25,6 +23,7 @@ su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
 su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
 su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
 
+
 # init.d support
 if [ ! -e /system/etc/init.d ]; then
    mkdir /system/etc/init.d
@@ -37,19 +36,8 @@ for FILE in /system/etc/init.d/*; do
    sh $FILE >/dev/null
 done;
 
-# Swappiness to 0
-echo "0" > /proc/sys/vm/swappiness
 
-# Custom bootanimation support
-if [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
-        /sbin/bootanimation &
-else
-        /system/bin/bootanimation &
-fi
-
-
-mount -o remount,ro -t auto /
+# Unmount
 mount -t rootfs -o remount,ro rootfs
-mount -o remount,ro -t auto /system
-mount -o remount,rw /data
-mount -o remount,rw /cache
+mount -o remount,rw -t auto /data
+mount -o remount,ro -t auto /systemhe

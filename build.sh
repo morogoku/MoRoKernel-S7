4dded258708b9699aabede2f49d93d59935f4011
morogoku <morogoku@hotmail.com>
2016-11-28 02:13:06 +0100

    build: v2.b5

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 export MODEL="G935F"
-export KERNEL_VERSION="MoRoKernel-S7-v2.b4"
+export KERNEL_VERSION="MoRoKernel-S7-v2.b5"
 export REVISION="RC"
 export KBUILD_VERSION="1"
 

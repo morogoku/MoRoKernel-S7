6fe28ef0b7e44392edd8532293c242371676c8d1
morogoku <morogoku@hotmail.com>
2017-01-22 04:15:14 +0100

    add build script & release folder

---
+++ build.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+
+# SETUP
+# -----
+
+export ARCH=arm64
+export SUBARCH=arm64
+#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linaro-6.3/bin/aarch64-
+#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-sabermod-7.0/bin/aarch64-
+#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-sabermod-5.4/bin/aarch64-
+export CROSS_COMPILE=$BUILD_CROSS_COMPILE
+export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+
+export MODEL="G935F"
+export KERNEL_VERSION="MoRoKernel-S7-Nougat-v0.1"
+export REVISION="RC"
+export KBUILD_VERSION="1"
+
+KERNEL_DEFCONFIG=moro-hero2lte_defconfig
+
+RDIR=$(pwd)
+OUTDIR=$RDIR/arch/$ARCH/boot
+DTSDIR=$RDIR/arch/$ARCH/boot/dts
+DTBDIR=$OUTDIR/dtb
+DTCTOOL=$RDIR/scripts/dtc/dtc
+INCDIR=$RDIR/include
+PAGE_SIZE=2048
+DTB_PADDING=0
+
+
+
+# FUNCTIONS
+# ---------
+FUNC_DELETE_PLACEHOLDERS()
+{
+	find . -name \.placeholder -type f -delete
+        echo "Placeholders Deleted from Ramdisk"
+        echo ""
+}
+
+FUNC_CLEAN_DTB()
+{
+	if ! [ -d $RDIR/arch/$ARCH/boot/dts ] ; then
+		echo "no directory : "$RDIR/arch/$ARCH/boot/dts""
+	else
+		echo "rm files in : "$RDIR/arch/$ARCH/boot/dts/*.dtb""
+		rm $RDIR/arch/$ARCH/boot/dts/*.dtb
+		rm $RDIR/arch/$ARCH/boot/dtb/*.dtb
+		rm $RDIR/arch/$ARCH/boot/boot.img-dtb
+		rm $RDIR/arch/$ARCH/boot/boot.img-zImage
+	fi
+}
+
+FUNC_BUILD_KERNEL()
+{
+	echo ""
+        echo "build common config="$KERNEL_DEFCONFIG ""
+        echo "build variant config="$MODEL ""
+	FUNC_CLEAN_DTB
+	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
+			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+			$KERNEL_DEFCONFIG || exit -1
+	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
+			CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
+	echo ""
+}
+
+FUNC_BUILD_DTB()
+{
+	[ -f "$DTCTOOL" ] || {
+		echo "You need to run ./build.sh first!"
+		exit 1
+	}
+
+	DTSFILES="exynos8890-hero2lte_eur_open_00 exynos8890-hero2lte_eur_open_01
+			exynos8890-hero2lte_eur_open_03 exynos8890-hero2lte_eur_open_04
+			exynos8890-hero2lte_eur_open_08"
+
+	mkdir -p $OUTDIR $DTBDIR
+	cd $DTBDIR || {
+		echo "Unable to cd to $DTBDIR!"
+		exit 1
+	}
+	rm -f ./*
+	echo "Processing dts files."
+	for dts in $DTSFILES; do
+		echo "=> Processing: ${dts}.dts"
+		${CROSS_COMPILE}cpp -nostdinc -undef -x assembler-with-cpp -I "$INCDIR" "$DTSDIR/${dts}.dts" > "${dts}.dts"
+		echo "=> Generating: ${dts}.dtb"
+		$DTCTOOL -p $DTB_PADDING -i "$DTSDIR" -O dtb -o "${dts}.dtb" "${dts}.dts"
+	done
+	echo "Generating dtb.img."
+	$RDIR/tools/dtbtool -o "$OUTDIR/dtb.img" -p "$DTBDIR/" -s $PAGE_SIZE
+	echo "Done."
+}
+
+FUNC_BUILD_RAMDISK()
+{
+	mv $RDIR/arch/$ARCH/boot/Image $RDIR/arch/$ARCH/boot/boot.img-zImage
+	mv $RDIR/arch/$ARCH/boot/dtb.img $RDIR/arch/$ARCH/boot/boot.img-dtb
+
+	rm -f $RDIR/ramdisk/split_img/boot.img-zImage
+	rm -f $RDIR/ramdisk/split_img/boot.img-dtb
+	mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/split_img/boot.img-zImage
+	mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/split_img/boot.img-dtb
+	cd $RDIR/ramdisk
+	./repackimg.sh
+	echo SEANDROIDENFORCE >> image-new.img
+
+}
+
+FUNC_BUILD_FLASHABLES()
+{
+	cp image-new.img $RDIR/release/zip/boot.img
+	cp image-new.img $RDIR/release/tar/boot.img
+
+	cd $RDIR
+	cd release/zip
+	zip -5 -r $KERNEL_VERSION-$MODEL-$REVISION$KBUILD_VERSION.zip *
+	rm boot.img
+	cd ..
+	cd tar
+	tar cf $KERNEL_VERSION-$MODEL-$REVISION$KBUILD_VERSION.tar boot.img && ls -lh $KERNEL_VERSION-$MODEL-$REVISION$KBUILD_VERSION.tar
+	rm boot.img
+
+}
+
+# MAIN PROGRAM
+# ------------
+(
+	sh ./clean.sh
+	START_TIME=`date +%s`
+	FUNC_DELETE_PLACEHOLDERS
+	FUNC_BUILD_KERNEL
+	FUNC_BUILD_DTB
+	FUNC_BUILD_RAMDISK
+	FUNC_BUILD_FLASHABLES
+	END_TIME=`date +%s`
+	let "ELAPSED_TIME=$END_TIME-$START_TIME"
+	echo "Total compile time is $ELAPSED_TIME seconds"
+	echo ""
+) 2>&1	 | tee -a ./build.log
+
+	echo "Your flasheable release can be found in the release/zip or tar folder"
+	echo ""
+
+
---
+++ clean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Cleaning Script written by djb77
+
+# Clean Build Data
+make clean
+make ARCH=arm64 distclean
+
+# Remove Build Log
+rm -f $PWD/build.log
+
+# Remove Created Ramdisk Files
+rm -f $PWD/ramdisk/image-new.img
+rm -f $PWD/ramdisk/ramdisk-new.cpio.gz
+rm -f $PWD/ramdisk/split_img/boot.img-dtb
+rm -f $PWD/ramdisk/split_img/boot.img-zImage
+
+# Remove Releasetool files
+rm -f $PWD/release/zip/*.zip
+rm -f $PWD/release/tar/*.tar
+
+# Removed Created dtb Folder
+rm -rf $PWD/arch/arm64/boot/dtb
+
+# Recreate Ramdisk Placeholders
+echo "" > ramdisk/ramdisk/acct/.placeholder
+echo "" > ramdisk/ramdisk/cache/.placeholder
+echo "" > ramdisk/ramdisk/data/.placeholder
+echo "" > ramdisk/ramdisk/dev/.placeholder
+echo "" > ramdisk/ramdisk/lib/modules/.placeholder
+echo "" > ramdisk/ramdisk/mnt/.placeholder
+echo "" > ramdisk/ramdisk/proc/.placeholder
+echo "" > ramdisk/ramdisk/storage/.placeholder
+echo "" > ramdisk/ramdisk/sys/.placeholder
+echo "" > ramdisk/ramdisk/system/.placeholder
+
+# Recreate Releasetools Placeholders
+echo "" > release/tar/.placeholder

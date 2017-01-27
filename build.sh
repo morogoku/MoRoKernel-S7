612f31fd655bfa26192a8f9c50219e430f32b51d
morogoku <morogoku@hotmail.com>
2017-01-27 03:19:32 +0100

    MoRoKernel 1.0

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 export MODEL="G935F"
-export KERNEL_VERSION="MoRoKernel-S7-Nougat-v0.7"
+export KERNEL_VERSION="MoRoKernel-S7-Nougat-v1.0"
 export REVISION="RC"
 export KBUILD_VERSION="1"
 

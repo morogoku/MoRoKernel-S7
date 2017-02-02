59f3a745c22d6866fc3f03f55e73277b82ae6028
morogoku <morogoku@hotmail.com>
2017-02-02 03:48:35 +0100

    adjust initial cpu min. freqs. A72->520MHz A53->130MHz

--- ramdisk/G935F/ramdisk/sbin/moro-init.sh
+++ ramdisk/G935F/ramdisk/sbin/moro-init.sh
@@ -66,9 +66,9 @@ $BB mount -o remount,rw /;
 
     # CPU freq. values
     echo 2600000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq;
-    echo 728000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq;
+    echo 520000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq;
     echo 1586000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-    echo 442000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+    echo 130000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
 
     # Led Fade-out
     echo 700 > /sys/class/sec/led/led_notification_ramp_down;

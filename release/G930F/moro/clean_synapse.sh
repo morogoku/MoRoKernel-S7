c7077bd8084b0ec8814a90f385d6b0aaceb99a7d
morogoku <morogoku@hotmail.com>
2017-02-07 03:23:33 +0100

    update magisk, busybox and fix aroma

--- release/G930F/moro/clean_synapse.sh
+++ release/G930F/moro/clean_synapse.sh
@@ -4,3 +4,11 @@
 # Remove Synapse
 rm -rf /data/app/com.af.synapse*
 rm -rf /data/data/com.af.synapse
+rm -rf /system/app/Synapse*
+rm -rf /system/app/synapse*
+rm -rf /system/priv-app/Synapse*
+rm -rf /system/priv-app/synapse*
+rm -rf /system/app/com.af.synapse*
+rm -rf /system/priv-app/com.af.synapse*
+
+
--- release/G935F/moro/clean_su.sh
+++ release/G935F/moro/clean_su.sh
@@ -10,6 +10,10 @@ rm -rf /cache/magisk.log /cache/last_magisk.log /cache/magiskhide.log \
        /cache/magisk /cache/magisk_merge /cache/magisk_mount /cache/unblock \
        /data/Magisk.apk /data/magisk.img /data/magisk_merge.img /data/stock_boot.img \
        /data/busybox /data/magisk /data/custom_ramdisk_patch.sh 2>/dev/null
+rm -rf /data/app/me.phh.superuser*
+rm -rf /system/app/Superuser*
+rm -rf /system/priv-app/Superuser*
+rm -rf /data/data/me.phh.superuser
 	   
 
 # SU

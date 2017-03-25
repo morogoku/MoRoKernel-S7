6ffe24fdb0a99cbd9db1ca3ba63f61c036c1b624
morogoku <morogoku@hotmail.com>
2017-03-25 14:10:33 +0100

    beta 1.4b7

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ else [ $prompt == "2" ]
     echo "S7 Edge G935F Selected"
 fi
 
-export KERNEL_VERSION="MoRoKernel-$MODEL-N-v1.4b1"
+export KERNEL_VERSION="MoRoKernel-$MODEL-N-v1.4b7"
 export REVISION="RC"
 export KBUILD_BUILD_VERSION="1"
 

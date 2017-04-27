cf5669f64752e808c5a7c4e0a9ca6378d8e444ef
morogoku <morogoku@hotmail.com>
2017-04-28 01:26:19 +0200

    clean release

--- release/G935F/moro/system_patch.sh
+++ release/G935F/moro/system_patch.sh
@@ -1,19 +1,4 @@
 #!/sbin/sh
-# Thanks to tkkg1994 & djb77
-# Modified by morogoku
-
-
-# Edit build.prop
-sed -i /security.mdpp.mass/d /system/build.prop
-sed -i /ro.hardware.keystore/d /system/build.prop
-
-
-# Delete wrongs files
-rm -rf /system/app/TuiService /system/app/mcRegistry
-rm -f /system/vendor/lib/libsecure_storage.so
-rm -f /system/vendor/lib/libsecure_storage_jni.so
-rm -f /system/vendor/lib64/libsecure_storage.so
-rm -f /system/vendor/lib64/libsecure_storage_jni.so
 
 
 # Delete Wakelock.sh 

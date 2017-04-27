ae6a54c7d17588b634d39eeddcacf7fe148c5371
Tkkg1994 <luca.grifo@outlook.com>
2017-04-28 01:23:53 +0200

    ramdisk: remove fake encryption (bricked OTG)

--- ramdisk/G930F/ramdisk/sbin/moro-init.sh
+++ ramdisk/G930F/ramdisk/sbin/moro-init.sh
@@ -39,7 +39,6 @@ $BB mount -o remount,rw /;
 /sbin/resetprop -n ro.boot.verifiedbootstate "green"
 /sbin/resetprop -n ro.boot.flash.locked "1"
 /sbin/resetprop -n ro.boot.ddrinfo "00000001"
-/sbin/resetprop -n ro.crypto.state "encrypted"
 
 
 #-------------------------

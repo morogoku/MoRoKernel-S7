6878cf8e8319de0d2473a39544671e1430632fff
morogoku <morogoku@hotmail.com>
2017-05-09 03:12:47 +0200

    fix release files

--- release/G930F/moro/system_patch.sh
+++ release/G930F/moro/system_patch.sh
@@ -1,5 +1,9 @@
 #!/sbin/sh
 
+rm -rf /system/app/mcRegistry
+rm -f /system/vendor/lib/libsecure_storage.so
+rm -f /system/vendor/lib64/libsecure_storage.so
+
 
 # Delete Wakelock.sh 
 rm -f /magisk/phh/su.d/wakelock*

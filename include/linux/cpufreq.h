90cd444385d088ce1732f7baa77f37b813b0b074
djb77 <dwayne.bakewell@gmail.com>
2017-09-03 17:12:12 +0200

    cpufreq: smartmax_eps Governor Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -522,6 +522,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
+extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
 #endif
 
 /*********************************************************************

039cde9fc67a39f2eb0d7751ff9fe3c32f3ed2fd
djb77 <dwayne.bakewell@gmail.com>
2017-09-04 01:18:00 +0200

    Add cafactive, version of interactive by CodeAurora Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com> Signed-off-by: Tkkg1994 <luca.grifo@outlook.com> Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -534,6 +534,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemand_x;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RELAXED)
 extern struct cpufreq_governor cpufreq_gov_relaxed;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_relaxed)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
+extern struct cpufreq_governor cpufreq_gov_cafactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
 #endif
 
 /*********************************************************************

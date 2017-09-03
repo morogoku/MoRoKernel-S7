d370ffd893875b0373507a5becfef92de4e9bd31
djb77 <dwayne.bakewell@gmail.com>
2017-09-03 18:21:20 +0200

    cpufreq: relaxed Governor Signed-off-by: djb77 <dwayne.bakewell@gmail.com>
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -513,6 +513,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_RELAXED)
+extern struct cpufreq_governor cpufreq_gov_relaxed;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_relaxed)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)

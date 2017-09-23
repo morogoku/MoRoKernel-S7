6f52d2c62aadc6c99341a4e580359954522cdec3
Tkkg1994 <luca.grifo@outlook.com>
2017-09-23 17:02:10 +0200

    uksm: remove warnings
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- mm/uksm.c
+++ mm/uksm.c
@@ -146,7 +146,7 @@ int memcmpx86_64(void *s1, void *s2, size_t n)
 	return res;
 }
 
-static int is_full_zero(const void *s1, size_t len)
+static int is_full_zero(void *s1, size_t len)
 {
 	unsigned char same;
 
@@ -164,7 +164,7 @@ static int is_full_zero(const void *s1, size_t len)
 
 #endif
 #else
-static int is_full_zero(const void *s1, size_t len)
+static int is_full_zero(void *s1, size_t len)
 {
 	unsigned long *src = s1;
 	int i;

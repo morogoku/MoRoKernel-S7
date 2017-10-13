4e660b5fa9ca386bc04afeaa012b69698b45dcc1
morogoku <morogoku@hotmail.com>
2017-10-13 19:04:50 +0200

    ramdisk: try to fix animated photo on old roms
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build/zip/common/moro/system.sh
+++ build/zip/common/moro/system.sh
@@ -1,11 +1,20 @@
 #!/sbin/sh
 #
-# 
+# MoRoKernel System script 1.0
+#
 
 cd /tmp/moro
 
+# Extract system
 tar -Jxf system.tar.xz
 
+# Copy system
 cp -rf system/. /system
+
+# Patch fingerprint
+rm -f /system/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin
+
+# Clean Apex data
+rm -rf /data/data/com.sec.android.app.apex
 
 

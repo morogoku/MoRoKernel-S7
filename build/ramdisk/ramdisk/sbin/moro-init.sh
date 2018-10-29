f3760e4cf5c509223cb6f8dbfea80903abcdfd8f
morogoku <morogoku@hotmail.com>
2018-10-29 02:24:16 +0100

    ramdisk: add init scripts
    Signed-off-by: morogoku <morogoku@hotmail.com>

---
+++ build/ramdisk/ramdisk/sbin/moro-init.sh
@@ -0,0 +1,60 @@
+#!/system/bin/sh
+# 
+# Init MoRoKernel
+#
+
+OLD_LOG="/data/morokernel.log"
+MORO_DIR="/data/.morokernel"
+LOG="$MORO_DIR/morokernel.log"
+
+rm -f $LOG
+rm -f $OLD_LOG
+
+BB="/sbin/busybox"
+RESETPROP="/sbin/magisk resetprop -v -n"
+
+
+# Mount
+$BB mount -t rootfs -o remount,rw rootfs
+$BB mount -o remount,rw /system
+$BB mount -o remount,rw /data
+$BB mount -o remount,rw /
+
+# Create morokernel folder
+if [ ! -d $MORO_DIR ]; then
+	$BB mkdir -p $MORO_DIR;
+fi
+
+
+(
+	$BB echo $(date) "MoRo-Kernel LOG" >> $LOG
+	$BB echo " " >> $LOG
+
+	# Selinux permissive
+	$BB echo "## -- Selinux permissive" >> $LOG
+	$BB echo "0" > /sys/fs/selinux/enforce 
+	$BB echo " " >> $LOG
+
+	# Fake Knox 0
+	$BB echo "## -- Fake Knox 0" >> $LOG
+	$RESETPROP ro.boot.warranty_bit "0"
+	$RESETPROP ro.warranty_bit "0"
+	$BB echo " " >> $LOG
+
+	# Samsung related flags
+	$BB echo "## -- Samsung Flags" >> $LOG
+	$RESETPROP ro.fmp_config "1"
+	$RESETPROP ro.boot.fmp_config "1"
+	$BB echo " " >> $LOG
+
+
+) 2>&1 | tee -a ./$LOG
+
+$BB chmod 777 $LOG
+
+# Unmount
+$BB mount -t rootfs -o remount,ro rootfs
+$BB mount -o remount,ro /system
+$BB mount -o remount,rw /data
+$BB mount -o remount,ro /
+

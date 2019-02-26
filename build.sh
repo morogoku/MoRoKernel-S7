ee89148cbb9c397d182064e0855bddabdd606980
morogoku <morogoku@hotmail.com>
2019-02-26 01:27:22 +0100

    add support to compile AOSP kernel
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build.sh
+++ build.sh
@@ -10,11 +10,6 @@
 export ARCH=arm64
 export SUBARCH=arm64
 export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-ubertc-5.4.1-20170929/bin/aarch64-linux-android-
-#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-ubertc-6.4.1-20170929/bin/aarch64-linux-android-
-#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-ubertc-7.2.1-20170929/bin/aarch64-linux-android-
-#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-ubertc-7.3.1-20180314/bin/aarch64-linux-android-
-#export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linaro-7.3/bin/aarch64-linux-gnu-
 export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
@@ -39,10 +34,7 @@ export K_BASE="ERL3"
 export K_NAME="MoRoKernel"
 export REVISION="RC"
 export KBUILD_BUILD_VERSION="1"
-S7DEVICE="OREO"
-EDGE_LOG=Edge_build.log
-FLAT_LOG=Flat_build.log
-PORT=0
+export KERNEL_VERSION="$K_NAME-$K_BASE-O-$K_VERSION"
 
 
 # FUNCTIONS
@@ -60,21 +52,27 @@ FUNC_CLEAN_DTB()
 		echo "no directory : "$RDIR/arch/$ARCH/boot/dts""
 	else
 		echo "rm files in : "$RDIR/arch/$ARCH/boot/dts/*.dtb""
-		rm $RDIR/arch/$ARCH/boot/dts/*.dtb
-		rm $RDIR/arch/$ARCH/boot/dtb/*.dtb
-		rm $RDIR/arch/$ARCH/boot/boot.img-dtb
-		rm $RDIR/arch/$ARCH/boot/boot.img-zImage
+		rm $RDIR/arch/$ARCH/boot/dts/*.dtb 2>/dev/null
+		rm $RDIR/arch/$ARCH/boot/dtb/*.dtb 2>/dev/null
+		rm $RDIR/arch/$ARCH/boot/boot.img-dtb 2>/dev/null
+		rm $RDIR/arch/$ARCH/boot/boot.img-zImage 2>/dev/null
 	fi
 }
 
 FUNC_BUILD_KERNEL()
 {
 	echo ""
-        echo "build common config="$KERNEL_DEFCONFIG ""
-        echo "build variant config="$MODEL ""
+        echo "Model: $MODEL"
+	echo "OS: $OS"
+        echo "build common config="$DEFCONFIG""
+        echo "build variant config="$KERNEL_DEFCONFIG""
 
 	cp -f $RDIR/arch/$ARCH/configs/$DEFCONFIG $RDIR/arch/$ARCH/configs/tmp_defconfig
 	cat $RDIR/arch/$ARCH/configs/$KERNEL_DEFCONFIG >> $RDIR/arch/$ARCH/configs/tmp_defconfig
+	
+	if [ $OS == "aosp" ]; then
+		sed -i 's/CONFIG_USB_ANDROID_SAMSUNG_MTP=y/# CONFIG_USB_ANDROID_SAMSUNG_MTP is not set/g' $RDIR/arch/$ARCH/configs/tmp_defconfig
+	fi
 
 	#FUNC_CLEAN_DTB
 
@@ -96,11 +94,20 @@ FUNC_BUILD_DTB()
 	}
 	case $MODEL in
 	G930)
-		DTSFILES="exynos8890-herolte_eur_open_04 exynos8890-herolte_eur_open_08
+		if [ $OS == "aosp" ]; then
+			DTSFILES="exynos8890-herolte_eur_open_aosp_04 exynos8890-herolte_eur_open_aosp_08
+				exynos8890-herolte_eur_open_aosp_09 exynos8890-herolte_eur_open_aosp_10"
+		else
+			DTSFILES="exynos8890-herolte_eur_open_04 exynos8890-herolte_eur_open_08
 				exynos8890-herolte_eur_open_09 exynos8890-herolte_eur_open_10"
+		fi
 		;;
 	G935)
-		DTSFILES="exynos8890-hero2lte_eur_open_04 exynos8890-hero2lte_eur_open_08"
+		if [ $OS == "aosp" ]; then
+			DTSFILES="exynos8890-hero2lte_eur_open_aosp_04 exynos8890-hero2lte_eur_open_aosp_08"
+		else
+			DTSFILES="exynos8890-hero2lte_eur_open_04 exynos8890-hero2lte_eur_open_08"
+		fi
 		;;
 	*)
 
@@ -130,63 +137,106 @@ FUNC_BUILD_RAMDISK()
 {
 	echo ""
 	echo "Building Ramdisk"
-	mv $RDIR/arch/$ARCH/boot/Image $RDIR/arch/$ARCH/boot/boot.img-zImage
-	mv $RDIR/arch/$ARCH/boot/dtb.img $RDIR/arch/$ARCH/boot/boot.img-dtb
+
+	if [ $OS == "tw" ]; then
+		
+		cd $RDIR/build
+		mkdir temp 2>/dev/null
+		cp -rf aik/. temp
+		cp -rf ramdisk/tw/. temp
+		rm -f temp/split_img/boot.img-zImage
+		rm -f temp/split_img/boot.img-dtb
+		mv $RDIR/arch/$ARCH/boot/Image temp/split_img/boot.img-zImage
+		mv $RDIR/arch/$ARCH/boot/dtb.img temp/split_img/boot.img-dtb
+		cd temp
+
+		if [ $MODEL == "G930" ]; then
+			sed -i 's/SRPOI30A000KU/SRPOI17A000KU/g' split_img/boot.img-board
+
+			sed -i 's/G935/G930/g' ramdisk/default.prop
+			sed -i 's/hero2/hero/g' ramdisk/default.prop
+		fi
+		
+		echo "Model: $MODEL, OS; $OS"
+
+		./repackimg.sh
+
+		echo SEANDROIDENFORCE >> image-new.img
+		mkdir $RDIR/build/kernel-temp 2>/dev/null
+		mv image-new.img $RDIR/build/kernel-temp/$MODEL-$OS-boot.img
+		rm -rf $RDIR/build/temp
 	
-	cd $RDIR/build
-	mkdir temp
-	cp -rf aik/. temp
-	cp -rf ramdisk/. temp
+	elif [ $OS == "aosp" ]; then
+		
+		mkdir $RDIR/build/kernel-temp 2>/dev/null
+		mkdir $RDIR/build/ramdisk-temp 2>/dev/null
+
+		mv $RDIR/arch/$ARCH/boot/Image $RDIR/build/kernel-temp/$MODEL-$OS-zImage
+		mv $RDIR/arch/$ARCH/boot/dtb.img $RDIR/build/kernel-temp/$MODEL-$OS-dtb
+
+		# Lineage 15
+		cd $RDIR/build
+		mkdir temp
+		cp -rf $RDIR/build/ramdisk/lin15/. temp
+		cd temp
+
+		if [ $MODEL == "G930" ]; then
+			sed -i 's/G935/G930/g' default.prop
+			sed -i 's/hero2/hero/g' default.prop
+		fi
+		
+		echo "Model: $MODEL, OS: $OS - Lineage 15"
+		find . | cpio -H newc -o > ../ramdisk-temp/$MODEL-lin15-ramdisk.cpio
+
+		rm -rf $RDIR/build/temp
+
+
+		# Lineage 16
+		cd $RDIR/build
+		mkdir temp
+		cp -rf $RDIR/build/ramdisk/lin16/. temp
+		cd temp
+
+		if [ $MODEL == "G930" ]; then
+			sed -i 's/G935/G930/g' default.prop
+			sed -i 's/hero2/hero/g' default.prop
+		fi
+
+		echo "Model: $MODEL, OS: $OS - Lineage 16"
+		find . | cpio -H newc -o > ../ramdisk-temp/$MODEL-lin16-ramdisk.cpio
+
+		rm -rf $RDIR/build/temp
 	
-	rm -f temp/split_img/boot.img-zImage
-	rm -f temp/split_img/boot.img-dtb
-	mv $RDIR/arch/$ARCH/boot/boot.img-zImage temp/split_img/boot.img-zImage
-	mv $RDIR/arch/$ARCH/boot/boot.img-dtb temp/split_img/boot.img-dtb
-	cd temp
-
-	case $MODEL in
-	G935)
-		echo "Ramdisk for G935"
-		;;
-	G930)
-		echo "Ramdisk for G930"
-
-		sed -i 's/SRPOI30A000KU/SRPOI17A000KU/g' split_img/boot.img-board
-
-		sed -i 's/G935/G930/g' ramdisk/default.prop
-		sed -i 's/hero2/hero/g' ramdisk/default.prop
-		;;
-	esac
-
-		echo "Done"
-
-	./repackimg.sh
-
-	cp -f image-new.img $RDIR/build
-	cd ..
-	rm -rf temp
-	echo SEANDROIDENFORCE >> image-new.img
-	mv image-new.img $MODEL-boot.img
+	else
+		echo ""
+		echo "No OS type defined, no ramdisk build"
+	fi
+	
 }
 
 FUNC_BUILD_FLASHABLES()
 {
 	cd $RDIR/build
-	mkdir temp2
-	cp -rf zip/common/. temp2
-    	mv *.img temp2/
-	cd temp2
+	mkdir temp
+	cp -rf zip/common/. temp
+	
+	cd $RDIR/build/kernel-temp
 	echo ""
 	echo "Compressing kernels..."
-	tar cv *.img | xz -9 > kernel.tar.xz
-	mv kernel.tar.xz moro/
-	rm -f *.img
+	tar cv * | xz -9 > ../temp/moro/kernel.tar.xz
 
+	if [ -d $RDIR/build/ramdisk-temp ]; then
+		cd $RDIR/build/ramdisk-temp
+		echo ""
+		echo "Compressing kernels..."
+		tar cv * | xz -9 > ../temp/moro/ramdisk.tar.xz
+	fi
+	
+	cd $RDIR/build/temp
 	zip -9 -r ../$ZIP_NAME *
 
 	cd ..
-    	rm -rf temp2
-
+	rm -rf temp kernel-temp ramdisk-temp
 }
 
 
@@ -203,31 +253,14 @@ MAIN()
 	FUNC_BUILD_KERNEL
 	FUNC_BUILD_DTB
 	FUNC_BUILD_RAMDISK
-	FUNC_BUILD_FLASHABLES
+	if [ $ZIP == "yes" ]; then
+	    FUNC_BUILD_FLASHABLES
+	fi
 	END_TIME=`date +%s`
 	let "ELAPSED_TIME=$END_TIME-$START_TIME"
 	echo "Total compile time is $ELAPSED_TIME seconds"
 	echo ""
-) 2>&1 | tee -a ./$LOG
-
-	echo "Your flasheable release can be found in the build folder"
-	echo ""
-}
-
-MAIN2()
-{
-
-(
-	START_TIME=`date +%s`
-	FUNC_DELETE_PLACEHOLDERS
-	FUNC_BUILD_KERNEL
-	FUNC_BUILD_DTB
-	FUNC_BUILD_RAMDISK
-	END_TIME=`date +%s`
-	let "ELAPSED_TIME=$END_TIME-$START_TIME"
-	echo "Total compile time is $ELAPSED_TIME seconds"
-	echo ""
-) 2>&1 | tee -a ./$LOG
+) 2>&1 | tee -a ./$MODEL-$OS-build.log
 
 	echo "Your flasheable release can be found in the build folder"
 	echo ""
@@ -244,49 +277,97 @@ echo ""
 echo ""
 echo "Build Kernel for:"
 echo ""
-echo "S7 Oreo"
+echo "S7 TW"
 echo "(1) S7 Flat SM-G930F"
 echo "(2) S7 Edge SM-G935F"
 echo "(3) S7 Edge + Flat"
+echo ""
+echo "S7 TW + AOSP"
+echo "(4) S7 Edge + Flat + AOSP"
+echo "(5) S7 Edge AOSP"
 echo ""
 echo ""
 read -p "Select an option to compile the kernel " prompt
 
 
 if [ $prompt == "1" ]; then
-    MODEL=G930
-    DEVICE=$S7DEVICE
-    KERNEL_DEFCONFIG=$DEFCONFIG_S7FLAT
-    LOG=$FLAT_LOG
-    export KERNEL_VERSION="$K_NAME-$K_BASE-O-$K_VERSION"
+
     echo "S7 Flat G930F Selected"
-    ZIP_NAME=$K_NAME-$MODEL-O-$K_VERSION.zip
-    MAIN
-elif [ $prompt == "2" ]; then
-    MODEL=G935
-    DEVICE=$S7DEVICE
-    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
-    LOG=$EDGE_LOG
-    export KERNEL_VERSION="$K_NAME-$K_BASE-O-$K_VERSION"
-    echo "S7 Edge G935F Selected"
-    ZIP_NAME=$K_NAME-$MODEL-O-$K_VERSION.zip
-    MAIN
-elif [ $prompt == "3" ]; then
-    MODEL=G935
-    DEVICE=$S7DEVICE
-    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
-    LOG=$EDGE_LOG
-    export KERNEL_VERSION="$K_NAME-$K_BASE-O-$K_VERSION"
-    echo "S7 EDGE + FLAT Selected"
-    echo "Compiling EDGE ..."
-    MAIN2
+
     MODEL=G930
+    OS=tw
     KERNEL_DEFCONFIG=$DEFCONFIG_S7FLAT
-    LOG=$FLAT_LOG
-    export KERNEL_VERSION="$K_NAME-$K_BASE-O-$K_VERSION"
-    echo "Compiling FLAT ..."
+    ZIP=yes
+    ZIP_NAME=$K_NAME-$MODEL-O-$K_VERSION.zip
+    MAIN
+	
+elif [ $prompt == "2" ]; then
+
+    echo "S7 Edge G935F Selected"
+
+    MODEL=G935
+    OS=tw
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
+    ZIP=yes
+    ZIP_NAME=$K_NAME-$MODEL-O-$K_VERSION.zip
+    MAIN
+	
+elif [ $prompt == "3" ]; then
+
+    echo "S7 EDGE + FLAT Selected"
+
+    MODEL=G935
+    OS=tw
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
+    ZIP=no
+    MAIN
+
+    MODEL=G930
+    OS=tw
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7FLAT
+    ZIP=yes
     ZIP_NAME=$K_NAME-G93X-O-$K_VERSION.zip
     MAIN
+	
+elif [ $prompt == "4" ]; then
+
+    echo "S7 Edge + Flat + AOSP"
+
+    MODEL=G935
+    OS=tw
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
+    ZIP=no
+    MAIN
+
+    MODEL=G930
+    OS=tw
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7FLAT
+    ZIP=no
+    MAIN
+
+    MODEL=G935
+    OS=aosp
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
+    ZIP=no
+    MAIN
+
+    MODEL=G930
+    OS=aosp
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7FLAT
+    ZIP=yes
+    ZIP_NAME=$K_NAME-G93X-O-$K_VERSION.zip
+    MAIN
+
+elif [ $prompt == "5" ]; then
+    MODEL=G935
+    OS=aosp
+    KERNEL_DEFCONFIG=$DEFCONFIG_S7EDGE
+    ZIP=yes
+    ZIP_NAME=$K_NAME-G935-O-$K_VERSION.zip
+    MAIN
 fi
+
+
+
 
 

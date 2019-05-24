72a58a1e972a6ac213260980db562b8326adebb9
morogoku <morogoku@hotmail.com>
2019-05-24 03:19:53 +0200

    v6.1b01

--- build.sh
+++ build.sh
@@ -29,8 +29,8 @@ DEFCONFIG=moro_defconfig
 DEFCONFIG_S7EDGE=moro-edge_defconfig
 DEFCONFIG_S7FLAT=moro-flat_defconfig
 
-export K_VERSION="v6.0.2"
-export K_BASE="ERL3"
+export K_VERSION="v6.1b02"
+export K_BASE="ESD2"
 export K_NAME="MoRoKernel"
 export REVISION="RC"
 export KBUILD_BUILD_VERSION="1"

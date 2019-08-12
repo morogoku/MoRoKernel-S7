8b1b12dd5f5c38f6baa11ce472ca7477b3a00dd5
morogoku <morogoku@hotmail.com>
2019-08-13 00:14:33 +0200

    Revert "selinux: Fix security labels for f2fs and sdcardfs"
    This reverts commit 726d1e0be4032d407ad31ddae987bdc985e7f23f.

--- security/selinux_n/avc.c
+++ security/selinux_n/avc.c
@@ -1052,6 +1052,7 @@ static noinline int avc_denied(u32 ssid, u32 tsid,
 	if (selinux_enforcing && !(avd->flags & AVD_FLAGS_PERMISSIVE))
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
+		return -EACCES;
 
 	avc_update_node(AVC_CALLBACK_GRANT, requested, driver, xperm, ssid,
 				tsid, tclass, avd->seqno, NULL, flags);

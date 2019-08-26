f2c3b49e358b712dd22ec0dbde05a15f98434764
morogoku <morogoku@hotmail.com>
2019-08-27 01:27:41 +0200

    ramdisk: fix spectrum profile check
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build/ramdisk/treblePie/ramdisk/init.spectrum.sh
+++ build/ramdisk/treblePie/ramdisk/init.spectrum.sh
@@ -3,6 +3,9 @@
 # Profile initialization script by nathanchance
 
 # If there is not a persist value, we need to set one
-if [ ! -f /data/property/persist.spectrum.profile ]; then
+if ! grep -q persist.spectrum.profile /data/property/persistent_properties; then
     setprop persist.spectrum.profile 0
 fi
+
+
+

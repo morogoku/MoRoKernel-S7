a072da81c2d04c74a9b76cffd3659b4b7d80bd16
morogoku <morogoku@hotmail.com>
2019-10-12 04:14:49 +0200

    Magisk 20.0 & Manager 7.3.5
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build/zip/common/moro/functions.sh
+++ build/zip/common/moro/functions.sh
@@ -24,7 +24,12 @@ clean_magisk() {
 	rm -rf /cache/*magisk* /cache/unblock /data/*magisk* /data/cache/*magisk* /data/property/*magisk* \
         /data/Magisk.apk /data/busybox /data/custom_ramdisk_patch.sh /data/app/com.topjohnwu.magisk* \
         /data/user*/*/magisk.db /data/user*/*/com.topjohnwu.magisk /data/user*/*/.tmp.magisk.config \
-        /data/adb/*magisk* 2>/dev/null
+        /data/adb/*magisk* /data/adb/post-fs-data.d /data/adb/service.d /data/adb/modules* 2>/dev/null
+        
+        if [ -f /system/addon.d/99-magisk.sh ]; then
+	  mount -o rw,remount /system
+	  rm -f /system/addon.d/99-magisk.sh
+	fi
 }
 
 abort() {

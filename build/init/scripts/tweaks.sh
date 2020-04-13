9e7cdd02dd9069c4f9513b5948970bd365031090
morogoku <morogoku@hotmail.com>
2020-04-13 18:45:37 +0200

    tweaks.sh: remove options
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build/init/scripts/tweaks.sh
+++ build/init/scripts/tweaks.sh
@@ -19,30 +19,7 @@ echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask
 echo "0" > /sys/module/lowmemorykiller/parameters/debug_level
 echo "0" > /sys/module/kernel/parameters/initcall_debug
 
-# CPU
-echo "338000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo "416000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 
-# HMP
-echo "600" > /sys/kernel/hmp/up_threshold
-echo "235" > /sys/kernel/hmp/down_threshold
-
-# Wakelock
-echo "3" > /sys/module/sec_battery/parameters/wl_polling
-echo "1" > /sys/module/sec_nfc/parameters/wl_nfc
-
-# Read Ahead
-echo "2048" > /sys/block/sda/queue/read_ahead_kb
-echo "2048" > /sys/block/mmcblk0/queue/read_ahead_kb
-
-# Misc Options
-echo "2" > /sys/kernel/power_suspend/power_suspend_mode
-echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
-
-# LMK
-echo "9694,19388,29082,38776,48470,58164" > /sys/module/lowmemorykiller/parameters/minfree
-   
-   
 echo "## -- End tweaks" >> $LOG;
 echo " " >> $LOG;
 

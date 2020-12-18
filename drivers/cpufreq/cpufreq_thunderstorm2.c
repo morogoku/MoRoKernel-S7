32cc3492cd50e412b037061db8b97ca9493af487
morogoku <morogoku@hotmail.com>
2020-12-18 21:23:26 +0100

    Revert "CPU govs: enhance power efficiency"
    This reverts commit 9cd9a30652de39d669e6ec43576ecc7a52bb21c8.

--- drivers/cpufreq/cpufreq_thunderstorm2.c
+++ drivers/cpufreq/cpufreq_thunderstorm2.c
@@ -734,7 +734,7 @@ static void cpufreq_thunderstorm2_adjust_cpu(unsigned int cpu,
 	}
 
 	if (max_freq != policy->cur) {
-		__cpufreq_driver_target(policy, max_freq, CPUFREQ_RELATION_C);
+		__cpufreq_driver_target(policy, max_freq, CPUFREQ_RELATION_H);
 		for_each_cpu(i, policy->cpus) {
 			pcpu = &per_cpu(cpuinfo, i);
 			pcpu->pol_hispeed_val_time = hvt;
